9bd117a56a63bc7594d42e9d1c0f6f63beec8bf2
Chand0410 <chandanamamillapalli04@gmail.com>
2025-09-25 16:04:03 -0400

    Add full Accounts application code

---
+++ accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.eazybytes.accounts.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
